580d6433f8244a9255d59b0473ceb5f9738a526f
Manoj N <manoj.1si22ci027@gmail.com>
2025-12-15 12:23:41 +0530

    Coding Challenge 14-Net Salary Calculations

---
+++ Net_Salary_calculations.py
@@ -0,0 +1,8 @@
+annual_gross_salary = float(input("Enter annual gross salary: "))
+total_tax_payable = float(input("Enter total tax payable: "))
+
+annual_net_salary = annual_gross_salary - total_tax_payable
+
+print("Annual Gross Salary:", annual_gross_salary)
+print("Total Tax Payable:", total_tax_payable)
+print("Annual Net Salary:", annual_net_salary)
