43ffbec8739d86011fa89078c0ba56983f62618d
Manoj N <manoj.1si22ci027@gmail.com>
2025-12-15 09:59:41 +0530

    Added initial coding challenges

---
+++ sum_and_average.py
@@ -0,0 +1,6 @@
+a=int(input("Enter the value of variable 1:"))
+b=int(input("Enter the value of Variable 2:"))
+sum=a+b
+average=sum/2
+print(f"Calculated Sum={sum}")
+print(f"Average of two numbers={average}")
